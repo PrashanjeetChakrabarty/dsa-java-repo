42a0915f834d13117489b6b4927fdd089940aca6
PrashanjeetChakrabarty <your-prashanjeetchakrabarty.com>
2026-02-18 23:34:33 +0530

    files added

---
+++ Main18.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main18{
+    public static void main(String[] args){
+        // Loops (for, while, do-while)
+
+        // for loop [ for (initialization; condition; updation) ]
+        
+        for(int counter = 0; counter < 7; counter = counter + 1){
+            System.out.println(" Prashanjeet Chakrabarty");
+        }
+
+    }
+}
---
+++ Main19.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main19{
+    public static void main(String[] args){
+        // Loops (for, while, do-while)
+
+        // for loop [ for (initialization; condition; updation) ]
+        
+        // increment operator = { counter++ } = { counter = counter + 1 }
+
+        for(int counter = 0; counter < 11; counter++){
+            System.out.println(counter);
+        }
+
+    }
+}

d51cb1df2e7c303fb55dba7e176cbac8684695d1
PrashanjeetChakrabarty <prashanjeetchakrabarty@gmail.com>
2026-02-24 23:04:20 +0530

    files added

--- Pattern7.java
+++ Pattern7.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
-// Half Pyramid Pattern with Numbers
+// Inverted Half Pyramid Pattern with Stars
 
 //        Print the Pattern
 
-//             1
-//             12
-//             123
-//             1234 
 //             12345
-
-public class Pattern6{
+//             1234
+//             123
+//             12
+//             1
+public class Pattern7{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -29,10 +28,12 @@ public class Pattern6{
         System.out.println("-----------------------------");
 
         for( int i = 1 ; i <= n ; i++){
-            for( int j = 1 ; j <= i ; j++){
+            for( int j = 1 ; j <= n - i + 1 ; j++){
                 System.out.print(j + " ");
             }
             System.out.println();
         }
+
+        
     }
 }

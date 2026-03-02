1322c0ab41a336708c7f31d05041570fb9f4291d
PrashanjeetChakrabarty <prashanjeetchakrabarty@gmail.com>
2026-03-02 20:47:40 +0530

    files added

---
+++ Pattern13.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+//        Print the Pattern [ Hollow Rhombus ]
+//
+//           *****
+//          *   *
+//         *   *
+//        *   *
+//       *****
+
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars and Hollow Part
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}

0c5707bdd60e327e24af9f4a2eef1bea9dd0b55d
PrashanjeetChakrabarty <prashanjeetchakrabarty@gmail.com>
2026-03-04 21:16:18 +0530

    files added

---
+++ Pattern15.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+//        Pattern Name :  Right Shifted Number Triangle
+//
+//        
+//
+//        Pattern:
+//
+//        1234
+//         234
+//          34
+//           4
+//
+
+public class Pattern15 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+
+            // Print spaces
+            for(int j = 1; j < i; j++){
+                System.out.print(" ");
+            }
+
+            // Print numbers
+            for(int j = i; j <= n; j++){
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}

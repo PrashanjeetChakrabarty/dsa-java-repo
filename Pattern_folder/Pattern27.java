1bef82e6f71a29976cb83a85a07ed616e9d587b8
PrashanjeetChakrabarty <prashanjeetchakrabarty@gmail.com>
2026-03-13 23:05:25 +0530

    files added

---
+++ Pattern_folder/Pattern27.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Pattern27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            // decreasing numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            // increasing numbers
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}

2c9b0c431403ad543888a5b6d5421a7be665234b
PrashanjeetChakrabarty <prashanjeetchakrabarty@gmail.com>
2026-03-26 22:51:05 +0530

    files added

--- Pattern_folder/Pattern41.java
+++ Pattern_folder/Pattern41.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
-public class Pattern40 {
+public class Pattern41 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        int mid = (n / 2) + 1;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i == 1 || i == n || j == 1 || j == n || i == j || j == n - i + 1) {
+                if (i == 1 || i == n || j == 1 || j == n || i == mid || j == mid) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");

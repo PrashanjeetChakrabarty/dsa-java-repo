39cedcb3b62201751df50de2e79b903417edea8c
PrashanjeetChakrabarty <prashanjeetchakrabarty@gmail.com>
2026-03-27 21:35:08 +0530

    files added

---
+++ Pattern_folder/Pattern42.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Pattern42 {
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (isPrime(j)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

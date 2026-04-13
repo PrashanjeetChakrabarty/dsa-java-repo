9ee9f2e293c038d3b4216188b0c91daf5d4207df
PrashanjeetChakrabarty <prashanjeetchakrabarty@gmail.com>
2026-04-13 21:53:50 +0530

    files added

---
+++ Function_folder/Function12.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Function12 {
+
+    public static void checkPrime(int num) {
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            System.out.println("Prime Number");
+        } else {
+            System.out.println("Not a Prime Number");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        checkPrime(n);
+    }
+}

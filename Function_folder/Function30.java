fe1720d3d2db5ad1a3cafba79ae226a6829b52ac
PrashanjeetChakrabarty <127608688+PrashanjeetChakrabarty@users.noreply.github.com>
2026-05-04 23:47:06 +0530

    Create Function30.java

---
+++ Function_folder/Function30.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Function30 {
+    public static boolean isPerfect(int num) {
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (isPerfect(n)) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not Perfect");
+        }
+        sc.close();
+    }
+}

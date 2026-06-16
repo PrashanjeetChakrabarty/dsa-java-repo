7554f2b0ab72dae2694b94529b315b2c54a19f4c
PrashanjeetChakrabarty <127608688+PrashanjeetChakrabarty@users.noreply.github.com>
2026-06-16 23:03:13 +0530

    Create Function69.java

---
+++ Function_folder/Function69.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Function69 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println("First Repeated Element = " + arr[i]);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No Repeated Element Found");
+        }
+
+        sc.close();
+    }
+}

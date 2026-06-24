bcc82a5b07e9932024b70d6227f7515f4a0bfdb0
PrashanjeetChakrabarty <127608688+PrashanjeetChakrabarty@users.noreply.github.com>
2026-06-24 22:41:01 +0530

    Create Function77.java

---
+++ Function_folder/Function77.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Function77 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of strings: ");
+        int n = sc.nextInt();
+
+        String arr[] = new String[n];
+
+        System.out.println("Enter strings:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        String prefix = arr[0];
+
+        for (int i = 1; i < n; i++) {
+
+            while (!arr[i].startsWith(prefix)) {
+
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Longest Common Prefix = " + prefix);
+
+        sc.close();
+    }
+}
